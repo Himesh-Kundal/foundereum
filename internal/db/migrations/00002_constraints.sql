596a01e7e55d63f939feceaada8673cd0de8d3d1
Himesh-Kundal <infernoemperor@proton.me>
2026-09-10 21:17:35 +0530

    fix(migrations): wrap 00002_constraints in goose StatementBegin/StatementEnd with idempotent pg check

--- internal/db/migrations/00002_constraints.sql
+++ internal/db/migrations/00002_constraints.sql
@@ -1,6 +1,20 @@
 -- +goose Up
-ALTER TABLE wallets ADD CONSTRAINT chk_wallets_usdc_nonnegative CHECK (usdc_balance >= 0);
-ALTER TABLE wallets ADD CONSTRAINT chk_wallets_hbar_nonnegative CHECK (hbar_balance >= 0);
+-- +goose StatementBegin
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_constraint WHERE conname = 'chk_wallets_usdc_nonnegative'
+    ) THEN
+        ALTER TABLE wallets ADD CONSTRAINT chk_wallets_usdc_nonnegative CHECK (usdc_balance >= 0);
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_constraint WHERE conname = 'chk_wallets_hbar_nonnegative'
+    ) THEN
+        ALTER TABLE wallets ADD CONSTRAINT chk_wallets_hbar_nonnegative CHECK (hbar_balance >= 0);
+    END IF;
+END $$;
+-- +goose StatementEnd
 
 -- +goose Down
 ALTER TABLE wallets DROP CONSTRAINT IF EXISTS chk_wallets_usdc_nonnegative;
